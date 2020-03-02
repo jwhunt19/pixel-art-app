718fb5f26eca7c307b9d232cc20004cfaefc8b48
jwhunt1 <jwhunt19@gmail.com>
2020-03-01 21:17:59 -0500

    Add functionality

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const gridCont = document.getElementById('grid-container');
+const colorMenu = document.getElementById('color-menu');
+const grids = document.getElementsByClassName('grid-item');
+const resetButton = document.getElementById('reset-button');
+let selected = 'black';
 
 gridCont.addEventListener('click', () =>{
     if(event.target.className === 'grid-item'){
-        event.target.style.backgroundColor = selected.style.backgroundColor;
+        event.target.style.backgroundColor = selected;
+    }
+});
+
+colorMenu.addEventListener('click', () =>{
+    if(event.target.className === 'color-grid-item'){
+        document.getElementById(selected).style.border = "";
+        selected = '';
+        event.target.style.border = "7px solid gold";
+        selected = event.target.id;
+    }
+});
+
+resetButton.addEventListener('click', () =>{
+    for(i = 0; i < grids.length; i++){
+        grids[i].style.backgroundColor = 'white';
     }
 });
